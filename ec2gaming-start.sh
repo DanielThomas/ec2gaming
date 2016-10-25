a47b1f227e94dfa7d7d3625af0a7afdc5920e5a0
Danny Thomas <dannyt@netflix.com>
2016-10-25 14:12:51 -0700

    Fix bootstrap configuration

--- ec2gaming-start.sh
+++ ec2gaming-start.sh
@@ -3,7 +3,7 @@
 set -e
 
 describe_gaming_image() {
-  aws ec2 describe-images --owner "$1" --filters Name=name,Values=ec2-gaming
+  aws ec2 describe-images --owner "$1" --filters Name=name,Values=ec2gaming
 }
 
 num_images() {
@@ -19,10 +19,10 @@ echo -n "Getting lowest g2.2xlarge bid... "
 PRICE="$(aws ec2 describe-spot-price-history --instance-types g2.2xlarge --product-descriptions "Windows" --start-time "$(date +%s)" | jq --raw-output '.SpotPriceHistory[].SpotPrice' | sort | head -1)"
 echo "$PRICE"
 
-echo -n "Looking for the ec2-gaming AMI... "
+echo -n "Looking for the ec2gaming AMI... "
 AMI_SEARCH=$(describe_gaming_image self)
 if [ "$(num_images "$AMI_SEARCH")" -eq "0" ]; then
-	echo "not found. Going into bootstrap mode... "
+	echo -n "not found. Going into bootstrap mode... "
   BOOTSTRAP=1
   AMI_SEARCH=$(describe_gaming_image 255191696678)
   if [ "$(num_images "$AMI_SEARCH")" -eq "0" ]; then
@@ -34,18 +34,18 @@ AMI_ID="$(echo "$AMI_SEARCH" | jq --raw-output '.Images[0].ImageId')"
 echo "$AMI_ID"
 
 echo -n "Looking for security groups... "
-EC2_SECURITY_GROUP_ID=$(describe_security_group ec2-gaming)
+EC2_SECURITY_GROUP_ID=$(describe_security_group ec2gaming)
 if [ -z "$EC2_SECURITY_GROUP_ID" ]; then
   echo -n "not found. Creating security groups... "
-  aws ec2 create-security-group --group-name ec2-gaming-rdp --description "EC2 Gaming RDP" > /dev/null
-  aws ec2 authorize-security-group-ingress --group-name ec2-gaming-rdp --protocol tcp --port 3389 --cidr "0.0.0.0/0"
+  aws ec2 create-security-group --group-name ec2gaming-rdp --description "EC2 Gaming RDP" > /dev/null
+  aws ec2 authorize-security-group-ingress --group-name ec2gaming-rdp --protocol tcp --port 3389 --cidr "0.0.0.0/0"
 
-  aws ec2 create-security-group --group-name ec2-gaming --description "EC2 Gaming" > /dev/null
-  aws ec2 authorize-security-group-ingress --group-name ec2-gaming --protocol tcp --port 1194 --cidr "0.0.0.0/0"
-  aws ec2 authorize-security-group-ingress --group-name ec2-gaming --protocol udp --port 1194 --cidr "0.0.0.0/0"
-  aws ec2 authorize-security-group-ingress --group-name ec2-gaming --protocol icmp --port 8 --cidr "0.0.0.0/0"
+  aws ec2 create-security-group --group-name ec2gaming --description "EC2 Gaming" > /dev/null
+  aws ec2 authorize-security-group-ingress --group-name ec2gaming --protocol tcp --port 1194 --cidr "0.0.0.0/0"
+  aws ec2 authorize-security-group-ingress --group-name ec2gaming --protocol udp --port 1194 --cidr "0.0.0.0/0"
+  aws ec2 authorize-security-group-ingress --group-name ec2gaming --protocol icmp --port 8 --cidr "0.0.0.0/0"
 
-  EC2_SECURITY_GROUP_ID=$(describe_security_group ec2-gaming-rdp)
+  EC2_SECURITY_GROUP_ID=$(describe_security_group ec2gaming-rdp)
 fi
 echo "$EC2_SECURITY_GROUP_ID"
 
@@ -80,7 +80,7 @@ if [ "$BOOTSTRAP" -eq "1" ]; then
   echo "remote host is $IP"
 
   echo "Starting Remote Desktop..."
-  open ec2-gaming.rdp
+  open ec2gaming.rdp
 else
   VPN_IP="10.8.0.1"
   echo -n "Generating RDP configuration... "
