bf3d8e63c6ba121a032bd1ab4388ba4beedc5499
Danny Thomas <dannyt@netflix.com>
2016-10-25 22:18:07 -0700

    Fix junction script

--- bootstrap/steamapps-junction.bat
+++ bootstrap/steamapps-junction.bat
@@ -1,4 +1,4 @@
 @echo off
-rd "C:\Program Files (x86)\Steam\steamapps"
+rmdir /Q /S  "C:\Program Files (x86)\Steam\steamapps"
 md Z:\SteamLibrary\steamapps
 cmd /c mklink /j "C:\Program Files (x86)\Steam\steamapps" Z:\SteamLibrary\steamapps
