df423de324ddff58c98d6092f5777d28963cb60e
Danny Thomas <dannyt@netflix.com>
2016-10-25 23:09:14 -0700

    Improve error handling

--- ec2gaming-instance.sh
+++ ec2gaming-instance.sh
@@ -4,7 +4,7 @@ set -e
 # Verify that the gaming stane actually exists (and that there's only one)
 INSTANCES=$(aws ec2 describe-instances --filters Name=instance-state-code,Values=16 Name=instance-type,Values=g2.2xlarge)
 if [ "$(echo "$INSTANCES" | jq '.Reservations | length')" -ne "1" ]; then
-    echo "didnt find exactly one instance!"
+    >&2 echo "didnt find an instance or there was not exactly one instance"
     exit 1
 fi
 echo "$INSTANCES" | jq --raw-output '.Reservations[0].Instances[0].InstanceId'
