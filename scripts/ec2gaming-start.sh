6ea4156b43613ff986df7d2aedb6f151405332ab
Danny Thomas <dannyt@netflix.com>
2016-10-29 21:54:34 -0700

    Add wait

--- scripts/ec2gaming-start.sh
+++ scripts/ec2gaming-start.sh
@@ -58,6 +58,7 @@ if ! aws iam get-instance-profile --instance-profile-name ec2gaming &> /dev/null
   aws iam put-role-policy --role-name "$PROFILE_NAME" --policy-name "$PROFILE_NAME" --policy-document file://ec2gaming-permissionpolicy.json > /dev/null
   aws iam create-instance-profile --instance-profile-name "$PROFILE_NAME"  > /dev/null
   aws iam add-role-to-instance-profile --instance-profile-name "$PROFILE_NAME" --role-name "$PROFILE_NAME" > /dev/null
+  aws iam wait instance-profile-exists --instance-profile-name ec2gaming
 fi
 INSTANCE_PROFILE_ARN=$(aws iam get-instance-profile --instance-profile-name ec2gaming | jq -r '.InstanceProfile.Arn')
 echo "$INSTANCE_PROFILE_ARN"
