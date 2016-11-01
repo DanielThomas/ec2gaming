275aa8fdc67f686f87b61048c318eb5afe7b6300
Benjamin Malley <benjamin.malley@gmail.com>
2016-10-31 22:24:18 -0700

    starting with tasks

--- ec2gaming.go
+++ ec2gaming.go
@@ -1,5 +1,36 @@
-package ec2gaming
+package main
+
+import (
+	"os"
+	"fmt"
+	"github.com/urfave/cli"
+)
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "ec2gaming"
+	app.Usage = "macOS EC2 Gaming with Steam In-Home Streaming"
+	app.Commands = []cli.Command{
+		{
+			Name: "start",
+			Usage: "starts instance, VPN and Steam",
+			Action: func(*cli.Context) error {
+				fmt.Println("start")
+				return nil
+
+			},
+		},
+		{
+			Name: "stop",
+			Usage: "stops instance & vpn",
+			Action: func(*cli.Context) error {
+				fmt.Println("stop")
+				return nil
+
+			},
+		},
+	}
+
+	app.Run(os.Args)
 
 }
