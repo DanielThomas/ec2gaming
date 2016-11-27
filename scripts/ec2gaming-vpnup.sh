64f0285ad0cfd4fb9410676b9808a5a717ccd43c
Euan Reid <euan@euanreid.com>
2016-11-26 18:10:05 -0800

    Removed -n to provide appropriate newline

--- scripts/ec2gaming-vpnup.sh
+++ scripts/ec2gaming-vpnup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source "$(dirname "$0")/ec2gaming.header"
 
-echo -n "Connecting VPN (you may see an authentication prompt)... "
+echo "Connecting VPN (you may see an authentication prompt)... "
 IP=$(./ec2gaming-ip.sh)
 AUTH=$(realpath "$(pwd)/../ec2gaming.auth")
 CONFIG_EXISTS=0
